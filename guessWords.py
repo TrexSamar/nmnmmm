3b904848cc40934fdb004ea3d63a2fe895a6a2f1
TrexSamar <75522798+TrexSamar@users.noreply.github.com>
2022-01-13 16:00:13 +0530

    Add files via upload

---
+++ guessWords.py
@@ -0,0 +1,7 @@
+def GuessWords():
+    fileName = input("Enter you guess: ")
+while chances < 5
+if guess == number: 
+    print("Congratulations you won!!!")
+else
+    print("You lose!!! The number is", number)
